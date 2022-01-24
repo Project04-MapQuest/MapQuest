9a19bd626b297579b5bbe8ccff2ac30e3b0bbd42
Mark <markhegarty.audio@gmail.com>
2022-01-24 18:04:07 -0500

    We have the map displayed along with a search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import DisplayMap from './DisplayMap';
 function App() {
+    const [lat, setLat] = useState('40.730610')
+    const [lng, setLng] = useState('-73.935242')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+                <div>
+                    Search
+                </div>
+            <DisplayMap
+            height='100vh'
+            width='100%'
+            center={[lat, lng]}
+            tileLayer={'map'}
+            zoom={10}
+            apiKey='AJEFdd4JGrnslno6l848Ejs3b6WAMJjq'
+            />
+        </div>
   );
 }
-
 export default App;
